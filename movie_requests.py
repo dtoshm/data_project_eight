f34b464777264021af3eb0af8947ab5121057ed7
Dan M <73975721+dtoshm@users.noreply.github.com>
2025-03-12 11:54:18 +0800

    0.5 - OMDb API Requested the movies from the OMDb API using the provided csv IMDB ids.

--- movie_requests.py
+++ movie_requests.py
@@ -1,3 +1,21 @@
 import requests
 from keys import api_key
 import csv
+
+
+res = requests.get(f"http://www.omdbapi.com/?apikey={api_key}")
+
+
+def res_omdb_data():
+    all_movies = []
+    with open('data/oscar_winners.csv', 'r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        for row in csv_reader:
+            ombd_res = requests.get(f"http://www.omdbapi.com/?apikey={api_key}&i={row[1]}").json()
+            all_movies.append(ombd_res)
+        return all_movies
+
+
+movies = res_omdb_data()
+print(movies)
