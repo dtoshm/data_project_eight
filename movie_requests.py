afc61e6bd750d5e21cfea07d801e4f3a842921e2
Dan M <73975721+dtoshm@users.noreply.github.com>
2025-03-12 13:26:33 +0800

    0.6 - Step 6 CSV Save Added a function to save the data from OMDb to the csv file in the required format. The modules prior to this project really helped. Focusing on just doing it successfully once made it easier to then just place the code instide a loop.

--- movie_requests.py
+++ movie_requests.py
@@ -17,5 +17,22 @@ def res_omdb_data():
         return all_movies
 
 
+def save_to_csv(data):
+    header = ['Movie Title', 'Runtime', 'Genre', 'Award Wins', 'Award Nominations', 'Box Office']
+    with open('data/movies.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        for movie in data:
+            title = movie['Title']
+            runtime = int(movie['Runtime'].replace(' min', ''))
+            genre = movie['Genre']
+            awards_data = [int(word) for word in movie['Awards'].split() if word.isdigit()]
+            award_wins = int(awards_data[1])
+            award_nominations = int(awards_data[2])
+            box_office = int(movie['BoxOffice'].replace('$', '').replace(',', ''))
+            info = [title, runtime, genre, award_wins, award_nominations, box_office]
+            writer.writerow(info)
+
+
 movies = res_omdb_data()
-print(movies)
+save_to_csv(movies)
