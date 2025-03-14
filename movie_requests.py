1dcab42f5f0ff241b052d28561e7c430219b2ced
Dan M <73975721+dtoshm@users.noreply.github.com>
2025-03-14 11:59:24 +0800

    1.2 - Movie Requests Overhaul Wow! The first extra credit question is great it sent my down a very deep hole for bug fixes and tinkering. I need to redo a lot of stuff.

--- movie_requests.py
+++ movie_requests.py
@@ -6,33 +6,39 @@ import csv
 res = requests.get(f"http://www.omdbapi.com/?apikey={api_key}")
 
 
-def res_omdb_data():
+def res_omdb_data(user_csv):
     all_movies = []
-    with open('data/oscar_winners.csv', 'r') as file:
+    with open(f'data/{user_csv}', 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader)
         for row in csv_reader:
-            ombd_res = requests.get(f"http://www.omdbapi.com/?apikey={api_key}&i={row[1]}").json()
-            all_movies.append(ombd_res)
-        return all_movies
+            try:
+                ombd_res = requests.get(f"http://www.omdbapi.com/?apikey={api_key}&i={row[1]}")
+                ombd_res.raise_for_status()
+                all_movies.append(ombd_res.json())
+            except requests.exceptions.RequestException as err:
+                print(f"Error for movie ID {row[1]}: {err}")
+    return all_movies
 
 
-def save_to_csv(data):
+def save_to_csv(user_csv, user_movies):
     header = ['Movie Title', 'Runtime', 'Genre', 'Award Wins', 'Award Nominations', 'Box Office']
-    with open('data/movies.csv', 'w') as f:
+    with open(f'data/{user_csv}', 'w') as f:
         writer = csv.writer(f)
         writer.writerow(header)
-        for movie in data:
-            title = movie['Title']
-            runtime = int(movie['Runtime'].replace(' min', ''))
-            genre = movie['Genre']
-            awards_data = [int(word) for word in movie['Awards'].split() if word.isdigit()]
-            award_wins = int(awards_data[1])
-            award_nominations = int(awards_data[2])
-            box_office = int(movie['BoxOffice'].replace('$', '').replace(',', ''))
-            info = [title, runtime, genre, award_wins, award_nominations, box_office]
-            writer.writerow(info)
+        for movie in user_movies:
+            try:
+                title = movie['Title']
+                runtime = int(movie['Runtime'].replace(' min', ''))
+                genre = movie['Genre']
+                awards_data = [int(word) for word in movie['Awards'].split() if word.isdigit()]
+                award_wins = int(awards_data[1])
+                award_nominations = int(awards_data[2])
+                box_office = int(movie['BoxOffice'].replace('$', '').replace(',', ''))
+                info = [title, runtime, genre, award_wins, award_nominations, box_office]
+                writer.writerow(info)
+            except (KeyError, ValueError, IndexError) as err:
+                print(f"There was an error saving the movie '{movie['Title']}': {err}")
 
 
-movies = res_omdb_data()
-save_to_csv(movies)
+
